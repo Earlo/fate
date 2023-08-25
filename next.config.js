dadcaf8b51abf5ca06ec0f7155526b6ab90410cb
Visa Pollari <visa.pollari@gmail.com>
2023-08-26 01:39:39 +0300

    config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,14 @@ const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
   },
+  async headers() {
+    return [
+      {
+        source: '/api/:path*',
+        headers: [{ key: 'Access-Control-Allow-Origin', value: '*' }],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
