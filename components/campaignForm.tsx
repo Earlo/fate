f8039001e52ddc60c5f5d85cbabf4146fd8b1bc8
Visa Pollari <visa.pollari@gmail.com>
2023-08-27 19:02:00 +0300

    create camp

--- components/campaignForm.tsx
+++ components/campaignForm.tsx
@@ -4,23 +4,75 @@ import FormContainer from './formContainer';
 import CloseButton from './generic/closeButton';
 import ImageUploader from './generic/imageUploader';
 import Checkbox from './generic/checkbox';
+import { CampaignT } from '@/schemas/campaign';
 import { useState, FormEvent } from 'react';
 
 interface CampaignFormProps {
   onClose?: () => void;
+  initialCampaign?: CampaignT;
+  state?: 'create' | 'edit' | 'view';
+  setCampaigns?: React.Dispatch<React.SetStateAction<CampaignT[]>>;
 }
 
-const CampaignForm: React.FC<CampaignFormProps> = ({ onClose }) => {
-  const [name, setName] = useState<string>('');
-  const [icon, setIcon] = useState<string>('');
-  const [description, setDescription] = useState<string>('');
-  const [publicCampaign, setPublicCampaign] = useState<boolean>(false);
+const CampaignForm: React.FC<CampaignFormProps> = ({
+  onClose,
+  initialCampaign,
+  state = 'create',
+  setCampaigns,
+}) => {
+  const [name, setName] = useState(initialCampaign?.name || '');
+  const [icon, setIcon] = useState<string>(initialCampaign?.icon || '');
+  const [description, setDescription] = useState(
+    initialCampaign?.description || '',
+  );
+  const [publicCampaign, setPublicCampaign] = useState(
+    initialCampaign?.public || false,
+  );
+  const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
-    //onSubmit({ name, icon, description, public: publicCampaign });
-    console.log('submitting');
     e.preventDefault();
+    setIsSubmitting(true);
+
+    const campaignData = {
+      name,
+      icon,
+      description,
+      public: publicCampaign,
+    };
+
+    const apiMethod = state === 'edit' ? 'PUT' : 'POST';
+    const apiUrl =
+      state === 'edit'
+        ? `/api/campaign/${initialCampaign?._id}`
+        : '/api/campaign';
+
+    const response = await fetch(apiUrl, {
+      method: apiMethod,
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(campaignData),
+    });
+
+    const data = await response.json();
+
+    if (setCampaigns) {
+      setCampaigns((prevCampaigns) => {
+        const index = prevCampaigns.findIndex(
+          (campaign) => campaign._id === data._id,
+        );
+        if (index !== -1) {
+          return [
+            ...prevCampaigns.slice(0, index),
+            data,
+            ...prevCampaigns.slice(index + 1),
+          ];
+        } else {
+          return [...prevCampaigns, data];
+        }
+      });
+    }
     if (onClose) onClose();
+    setIsSubmitting(false);
   };
 
   return (
@@ -51,7 +103,7 @@ const CampaignForm: React.FC<CampaignFormProps> = ({ onClose }) => {
         checked={publicCampaign}
         onChange={(e) => setPublicCampaign(e.target.checked)}
       />
-      <Button label="Create Campaign" type="submit" />
+      <Button label="Create Campaign" type="submit" disabled={isSubmitting} />
     </FormContainer>
   );
 };
