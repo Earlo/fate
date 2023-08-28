924763b9b50a149c686b8975dd8923ff069b7031
Visa Pollari <visa.pollari@gmail.com>
2023-08-28 23:07:48 +0300

    toggle charachter

--- components/dashboard/faction.tsx
+++ components/dashboard/faction.tsx
@@ -2,7 +2,9 @@ import CharacterButton from './characterButton';
 import Input from '../generic/input';
 import Button from '@/components/generic/button';
 import { PopulatedFaction } from '@/schemas/campaign';
-import { useState } from 'react';
+import { CharacterSheetT } from '@/schemas/sheet';
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
 
 interface FactionProps {
   faction: PopulatedFaction;
@@ -11,8 +13,24 @@ interface FactionProps {
 }
 
 const Faction: React.FC<FactionProps> = ({ faction, isAdmin, onChange }) => {
+  const { data: session } = useSession();
   const [isEditing, setIsEditing] = useState(false);
   const [newName, setNewName] = useState(faction.name);
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  // Global state? Or not
+  const [allCharacters, setAllCharacters] = useState<CharacterSheetT[]>([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      if (session) {
+        const response = await fetch(`/api/sheets?id=${session.user._id}`);
+        if (response.status === 200) {
+          const data = await response.json();
+          setAllCharacters(data);
+        }
+      }
+    };
+    fetchData();
+  }, [session]);
 
   const handleSave = () => {
     setIsEditing(false);
@@ -27,6 +45,23 @@ const Faction: React.FC<FactionProps> = ({ faction, isAdmin, onChange }) => {
 
   const toggleProperty = (property: 'public' | 'visible') => {
     const updatedFaction = { ...faction, [property]: !faction[property] };
+    onChange(updatedFaction);
+  };
+
+  const toggleCharacter = (characterId: string) => {
+    const charIndex = faction.characters.findIndex(
+      (c) => c.sheet._id === characterId,
+    );
+    const updatedCharacters = [...faction.characters];
+    if (charIndex > -1) {
+      updatedCharacters.splice(charIndex, 1);
+    } else {
+      const character = allCharacters.find((c) => c._id === characterId);
+      if (character) {
+        updatedCharacters.push({ sheet: character, visible: true });
+      }
+    }
+    const updatedFaction = { ...faction, characters: updatedCharacters };
     onChange(updatedFaction);
   };
 
@@ -77,8 +112,25 @@ const Faction: React.FC<FactionProps> = ({ faction, isAdmin, onChange }) => {
       {faction.public && (
         <Button
           label="Add character"
-          onClick={() => console.log('Add character', faction.name)}
+          onClick={() => setIsDropdownOpen(!isDropdownOpen)}
         />
+      )}
+      {isDropdownOpen && (
+        <div className="character-dropdown">
+          {allCharacters.map((character) => (
+            <div key={character._id}>
+              <input
+                type="checkbox"
+                checked={faction.characters.some(
+                  (factionCharacter) =>
+                    factionCharacter.sheet._id === character._id,
+                )}
+                onChange={() => toggleCharacter(character._id)}
+              />
+              {character.name.text}
+            </div>
+          ))}
+        </div>
       )}
     </div>
   );
