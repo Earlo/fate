338a5b32635d787046f62a6a6366b4f71f50b156
Visa Pollari <visa.pollari@gmail.com>
2023-09-10 21:38:13 +0300

    semantic

--- components/sheet/visibilityToggle.tsx
+++ components/sheet/visibilityToggle.tsx
@@ -8,17 +8,24 @@ interface VisibilityToggleProps {
 const VisibilityToggle: React.FC<VisibilityToggleProps> = ({
   visible,
   onChange,
-}) =>
-  visible ? (
-    <EyeIcon
-      className="h-6 w-6 text-gray-400 hover:text-gray-500"
-      onClick={() => onChange(false)}
-    />
-  ) : (
-    <EyeSlashIcon
-      className="h-6 w-6 text-gray-400 hover:text-gray-500"
-      onClick={() => onChange(true)}
-    />
-  );
+}) => (
+  <button
+    type="button"
+    onClick={() => onChange(!visible)}
+    className="focus:outline-none"
+  >
+    {visible ? (
+      <EyeIcon
+        aria-hidden="true"
+        className="h-6 w-6 cursor-pointer text-gray-400 hover:text-gray-500"
+      />
+    ) : (
+      <EyeSlashIcon
+        aria-hidden="true"
+        className="h-6 w-6 cursor-pointer text-gray-400 hover:text-gray-500"
+      />
+    )}
+  </button>
+);
 
 export default VisibilityToggle;
