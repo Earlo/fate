89651b0d94b348f5c5fe733affed129cca0c328a
Visa Pollari <visa.pollari@gmail.com>
2023-09-13 00:24:03 +0300

    label cool

--- components/generic/label.tsx
+++ components/generic/label.tsx
@@ -6,10 +6,16 @@ interface LabelProps {
 
 const Label: React.FC<LabelProps> = ({ label, name, children }) => (
   <label
-    className="flex w-full items-end justify-between whitespace-nowrap bg-black p-1 text-xl font-black uppercase text-white"
-    style={{ fontFamily: "'Archivo Black', sans-serif", fontSize: '22px' }}
+    className="flex w-full items-end justify-between whitespace-nowrap bg-black p-1 pl-4 text-xl font-black uppercase text-white"
+    style={{
+      fontFamily: "'Archivo Black', sans-serif",
+      fontSize: '22px',
+      clipPath:
+        'polygon(1rem 0%, 100% 0%, 100% calc(100% - 1rem), calc(100% - 1rem) 100%, 0% 100%, 0% 1rem)',
+    }}
     htmlFor={name}
   >
+    {' '}
     {label ? label : name}
     {children}
   </label>
