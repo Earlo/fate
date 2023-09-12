57dcd6ef9e1418ed5083ad2956a2ea93d665dee5
Visa Pollari <visa.pollari@gmail.com>
2023-09-13 02:42:52 +0300

    fonts

--- components/generic/label.tsx
+++ components/generic/label.tsx
@@ -8,8 +8,6 @@ const Label: React.FC<LabelProps> = ({ label, name, children }) => (
   <label
     className="flex w-full items-end justify-between whitespace-nowrap bg-black p-1 pl-4 text-xl font-black uppercase text-white"
     style={{
-      fontFamily: "'Archivo Black', sans-serif",
-      fontSize: '22px',
       clipPath:
         'polygon(1rem 0%, 100% 0%, 100% calc(100% - 1rem), calc(100% - 1rem) 100%, 0% 100%, 0% 1rem)',
     }}
