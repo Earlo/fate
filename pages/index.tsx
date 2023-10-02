ee28dec74aef606587f81629b3e658720a0cd47e
Visa Pollari <visa.pollari@gmail.com>
2023-10-03 02:58:52 +0300

    animate

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,13 +4,87 @@ import LoadingSpinner from '@/components/generic/loadingSpinner';
 import { useRouter } from 'next/router';
 import { useSession } from 'next-auth/react';
 import Head from 'next/head';
+import { useEffect, useRef } from 'react';
 
 export default function Home() {
   const { data: session, status } = useSession();
   const router = useRouter();
+  const containerRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+    const squares = Array.from(
+      container.querySelectorAll('.animate-float'),
+    ) as HTMLElement[];
+    // Initialize squares with random positions, sizes, and angles
+    const squareStates = squares.map((el) => {
+      const initialX = Math.random() * window.innerWidth;
+      const initialY = Math.random() * window.innerHeight;
+      const size = 40;
+      const angle = Math.random() * Math.PI * 2;
+      const rotation = Math.random() * 10 - 5;
+
+      el.style.left = `${initialX}px`;
+      el.style.top = `${initialY}px`;
+      el.style.width = `${size}px`;
+      el.style.height = `${size}px`;
+
+      return { el, angle, rotation };
+    });
+
+    const updatePosition = ({
+      el,
+      angle,
+      rotation,
+    }: {
+      el: HTMLElement;
+      angle: number;
+      rotation: number;
+    }) => {
+      const x = parseFloat(el.style.left || '0');
+      const y = parseFloat(el.style.top || '0');
+      const distance = 2;
+
+      let newX = x + Math.cos(angle) * distance;
+      let newY = y + Math.sin(angle) * distance;
+
+      // Boundary checks
+      if (newX > window.innerWidth) newX = -50;
+      if (newX < -50) newX = window.innerWidth;
+      if (newY > window.innerHeight) newY = -50;
+      if (newY < -50) newY = window.innerHeight;
+
+      el.style.left = `${newX}px`;
+      el.style.top = `${newY}px`;
+      el.style.transform = `rotate(${rotation}deg)`;
+    };
+
+    const animate = () => {
+      squareStates.forEach((state) => {
+        updatePosition(state);
+        state.angle += Math.random() * 0.01 - 0.005; // Slight random curve
+        state.rotation += Math.random() * 0.2 - 0.1; // Slight random rotation
+      });
+      requestAnimationFrame(animate);
+    };
+
+    animate();
+  }, []);
 
   const LandingPage = () => (
-    <BaseLayout className="bg-gradient-to-r from-blue-400 to-purple-500 p-20 text-white">
+    <BaseLayout className="relative overflow-hidden bg-gradient-to-r from-blue-400 to-purple-500 text-white">
+      <div
+        ref={containerRef}
+        className="pointer-events-none absolute left-0 top-0 h-full w-full"
+      >
+        {Array.from(Array(100).keys()).map((_, i) => (
+          <div
+            key={i}
+            className="animate-float absolute justify-center border-2 border-slate-950 bg-red-200 text-center align-middle font-fate font-bold text-red-500"
+          />
+        ))}
+      </div>
       <div className="container mx-auto text-center">
         <h1 className="mb-5 text-6xl font-extrabold">
           {/* Thanks for the name suggestion github copilot */}
