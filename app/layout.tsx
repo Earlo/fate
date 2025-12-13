1ff8256f4f435ea19aaae7678688e3872e59c2f9
Visa P <visa.pollari@gmail.com>
2025-12-13 21:53:03 +0200

    fonts locally

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import Footer from '@/components/layout/footer';
 import TopBar from '@/components/layout/topBar';
 import '@/styles/globals.css';
 import type { Metadata } from 'next';
-import { Archivo, Archivo_Black } from 'next/font/google';
+import localFont from 'next/font/local';
 import type { ReactNode } from 'react';
 import Provider from './providers';
 import UserProvider from './userProvider';
@@ -13,23 +13,42 @@ export const metadata: Metadata = {
     'A tool for creating and managning Fate Core style character sheets. The Fate Core font is © Evil Hat Productions, LLC and is used with permission. The Four Actions icons were designed by Jeremy Keller.',
 };
 
-const archivo = Archivo({
-  weight: ['400', '700'],
+const archivo = localFont({
+  src: [
+    {
+      path: '../public/fonts/archivo/Archivo-Regular.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+    {
+      path: '../public/fonts/archivo/Archivo-Bold.ttf',
+      weight: '700',
+      style: 'normal',
+    },
+  ],
   display: 'optional',
-  subsets: ['latin-ext'],
   variable: '--font-archivo',
 });
-const archivoBlack = Archivo_Black({
-  weight: '400',
+
+const archivoBlack = localFont({
+  src: [
+    {
+      path: '../public/fonts/archivo/Archivo-Black.ttf',
+      weight: '400',
+      style: 'normal',
+    },
+  ],
   display: 'swap',
-  subsets: ['latin-ext'],
   variable: '--font-archivo-black',
 });
 
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en" className={`${archivo.variable} ${archivoBlack.className}`}>
-      <body className="flex min-h-[100dvh] flex-col">
+    <html
+      lang="en"
+      className={`${archivo.variable} ${archivoBlack.variable}`}
+    >
+      <body className="font-archivo flex min-h-[100dvh] flex-col">
         <Provider>
           <UserProvider>
             <TopBar />
